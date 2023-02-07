79bf190709a7bfae9b7483d2524a8433de7e6e69
rizqanmr <rizqan.mubarak@alodokter.com>
2023-02-07 17:10:13 +0700

    add logging interceptor

--- app/src/main/java/com/rizqanmr/jetpack/retrofitexample/MainActivity.kt
+++ app/src/main/java/com/rizqanmr/jetpack/retrofitexample/MainActivity.kt
@@ -13,24 +13,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var retService : ApiService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val retService = RetrofitInstance.getRetrofitInstance()
+        retService = RetrofitInstance.getRetrofitInstance()
             .create(ApiService::class.java)
+        getRequestWithQueryParameters()
+        //getRequestWithPathParameters()
 
-        //path parameter example
-//        val pathResponse : LiveData<Response<AlbumItem>> = liveData {
-//            val response = retService.getAlbum(3)
-//            emit(response)
-//        }
-//        pathResponse.observe(this, Observer {
-//            val title = it.body()?.title
-//            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
-//        })
+    }
 
+    private fun getRequestWithQueryParameters() {
         val responseLiveData: LiveData<Response<AlbumEntity>> = liveData {
-            val response = retService.getAlbums()
+            val response = retService.getSortedAlbums(3)
             emit(response)
         }
         responseLiveData.observe(this, Observer {
@@ -39,11 +35,22 @@ class MainActivity : AppCompatActivity() {
                 while (albumsList.hasNext()) {
                     val albumsItem = albumsList.next()
                     val result = " "+"Album Title : ${albumsItem.title}"+"\n"+
-                                    " "+"Album id : ${albumsItem.id}"+"\n"+
-                                    " "+"User id : ${albumsItem.userId}"+"\n\n\n"
+                            " "+"Album id : ${albumsItem.id}"+"\n"+
+                            " "+"User id : ${albumsItem.userId}"+"\n\n\n"
                     text_view.append(result)
                 }
             }
+        })
+    }
+
+    private fun getRequestWithPathParameters() {
+        val pathResponse : LiveData<Response<AlbumItem>> = liveData {
+            val response = retService.getAlbum(3)
+            emit(response)
+        }
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
         })
     }
 }
