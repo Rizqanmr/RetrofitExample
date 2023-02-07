cce3f75b32f7a7665be09e0a51035a4266686cb4
rizqanmr <rizqan.mubarak@alodokter.com>
2023-02-07 15:16:02 +0700

    query parameters

--- app/src/main/java/com/rizqanmr/jetpack/retrofitexample/ApiService.kt
+++ app/src/main/java/com/rizqanmr/jetpack/retrofitexample/ApiService.kt
@@ -3,8 +3,12 @@ package com.rizqanmr.jetpack.retrofitexample
 import com.rizqanmr.jetpack.retrofitexample.data.entity.AlbumEntity
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("/albums")
     suspend fun getAlbums() : Response<AlbumEntity>
+
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId") userId: Int) : Response<AlbumEntity>
 }
